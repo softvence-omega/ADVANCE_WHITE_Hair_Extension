c0f1c7bf75d2f57310f6f182601fbc75643687d3
Roksana Akter <roksana18cse04@gmail.com>
2025-08-04 11:24:29 +0600

    DataPreprocess

--- app/services/color_utils.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-def average_lab_from_mask(image, mask):
-    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-    pixels = lab[mask > 0]
-    if len(pixels) == 0:
-        return [0, 0, 0]
-    return np.mean(pixels, axis=0).tolist()
